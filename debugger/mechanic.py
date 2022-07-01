c1295adb91affd93fe7de5b5f4dfdfc0c08f985d
cas1m1r <50683460+cas1m1r@users.noreply.github.com>
2022-06-30 21:12:43 -0400

    improved

--- debugger/mechanic.py
+++ debugger/mechanic.py
@@ -1,38 +1,43 @@
+from seeker import swap, cmd, random_filename
 import random
 import string
+import time 
+import sys 
 import os
 
 letters = list(string.ascii_lowercase + string.ascii_uppercase)
 
-def random_filename(ext):
-	return f'{"".join(random.sample(letters, 6))}.{ext}'
-
-def cmd(command, verbose):
-	tmpf = random_filename('txt')
-	os.system(f'{command} > {tmpf}')
-	result = []
-	for ln in open(tmpf, 'r').readlines():
-		result.append(ln.replace('\n',''))
-	os.remove(tmpf)
-	return result
-
-def disassemble(fname):
-	disass = {}
+def disassemble(fname, verbose):
+	disass = {'functions': {},'asm': {}}
 	dump = cmd(f'objdump -D {fname}', False)
-	for line in dump:
+	for line in dump.split('\n'):
 		try:
 			fields = line.split('\t')
 			offset = int(fields[0].split(':')[0].replace(' ',''),16)
-			opcodes = fields[0].replace('    ','').split(':')[1]
-			instructions = fields[-2:]
-			disass[offset] = [opcodes, instructions]
+			instructions = ' '.join(fields[-2:])
+			disass['asm'][offset] = instructions
 		except ValueError:
 			pass
+		if line.find('<.')>0 and line.find('>:')>0:
+			fcname = line[line.find('<.')+1:line.find('>:')]
+			offset = int(line.split(' ')[0],16)
+			if verbose:
+				print(f'[+] Found {fcname} at {hex(offset)}')
+			disass['functions'][fcname] = offset
+		elif line.find('<_')>0 and line.find('>:')>0:
+			fcname = line[line.find('<')+1:line.find('>:')]
+			offset = int(line.split(' ')[0],16)
+			if verbose:
+				print(f'[+] Found {fcname} at {hex(offset)}')
+			disass['functions'][fcname] = offset
+		#TODO: should also know when the function ends
 	return disass
 
-def explore_strings(fname):
-	if not os.path.isfile(fname):
-		print(f'[!] Cannot fine {fname}')
-		return []
-	else:
-		return [line for line in cmd(f'strings {fname}',False)]
+def see_asm_range(dis, start, stop, verbose):
+	snippet = ''
+	if start in dis['asm'].keys() and stop in dis['asm'].keys():
+		for offset in dis['asm']:
+			if start <= offset <= stop:
+				snippet += dis['asm'][offset]
+				print(dis['asm'][offset])
+	return snippet
